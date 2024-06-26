f357c015fddb4ceb26139d8a07146b19d7369253
maebo <maeboittiaux@gmail.com>
2024-06-26 16:14:49 +0100

    Remove Square component own stateful tracking of 'value' and update to receive 'value' prop from Board component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,7 @@
 import { useState } from 'react';
 
-function Square() {
-  /* value : stores the value */
-  /* setValue : a function to change the value */
-  const [value, setValue] = useState(null);
-
-  function handleClick() {
-    setValue('X');
-  }
-
-  return (
-    <button
-      className="square"
-      onClick={handleClick}
-    >
-      {value}
-    </button>
-  );
+function Square({ value }) {
+  return <button className='square'>{value}</button>;
 }
 
 export default function Board() {
